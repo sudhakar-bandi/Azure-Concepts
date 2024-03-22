92b3b6fe20f5f1f66e8fb32b2c3982fd7433ddcd
Sudhakar Bandi <Sudhakar.Bandi@uwe.ac.uk>
2024-03-22 16:22:52 +0000

    Initial build

---
+++ Azure-Functions/HttpTrigger1.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class HttpTrigger1
+    {
+        private readonly ILogger<HttpTrigger1> _logger;
+
+        public HttpTrigger1(ILogger<HttpTrigger1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("HttpTrigger1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            string name = "";
+            if(req.QueryString.HasValue)
+            {
+             name=req.Query["Name"];            
+            }
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!" + name);
+        }
+    }
+}
